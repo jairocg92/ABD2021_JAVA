4a3dd361bb2a26cc81fcd783f024c8edc1b6d657
Alberto <albertogv93@correo.ugr.es>
2021-04-11 20:32:33 +0200

    Hasta la polla

--- src/abd_p1/Main.java
+++ src/abd_p1/Main.java
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Probando");
+        System.out.print("Alberto GV");
     }
     
 }
