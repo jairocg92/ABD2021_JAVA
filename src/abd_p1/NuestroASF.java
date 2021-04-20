8d7f5b62211139ef8957eea446fba780292d6d74
jairocg92 <jairocidg92@gmail.com>
2021-04-20 20:50:14 +0200

    1.1

--- src/abd_p1/NuestroASF.java
+++ src/abd_p1/NuestroASF.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package abd_p1;
 
 import java.io.BufferedReader;
@@ -12,43 +7,82 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-/**
- *
- * @author Alberto
- */
+
 public class NuestroASF {
-    
+
+    // CABECERA
+    private byte tam_cabecera, tam_cab_reg, num_atrib; 
+    private byte tam_bloq, tam_cab_bloq, num_reg;
+    private byte [] sec_tipo_atributos;
+    private byte [] sec_tam_tipo_atributos; 
+    // -------------------
+
+    // RESTO DE DATOS MIEMBRO
     private ArrayList<NuestroRegistro> registros;
     private int pos_actual;
-    private BufferedReader flujo;
+    private BufferedReader bf;
+    // --------------------
 
     public NuestroASF() {
+        // INICIALIZACION METADATOS
+        tam_cabecera=16;
+        tam_cab_reg=2;
+        num_atrib=5;
         registros = new ArrayList<NuestroRegistro>();
         pos_actual = 0;
+        
+        // SEGUN EL PDF -> 1 para logico, 2 para caracter, 3 para entero, 4 para real y 5 para cadena de caracteres
+        sec_tipo_atributos = new byte[num_atrib];
+        sec_tipo_atributos[0]=5;
+        sec_tipo_atributos[1]=3;
+        sec_tipo_atributos[2]=3;
+        sec_tipo_atributos[3]=4;
+        sec_tipo_atributos[4]=4;
+    
+        // ARRAY DE CHAR TAM 40, 2 INT, 2 DOUBLE
+        sec_tam_tipo_atributos = new byte[num_atrib];
+        sec_tam_tipo_atributos[0]= 40;
+        sec_tam_tipo_atributos[1]= 4;
+        sec_tam_tipo_atributos[2]= 4;
+        sec_tam_tipo_atributos[4]= 8;
+        sec_tam_tipo_atributos[5]= 8;
     }
     
     public void open(String ruta) throws FileNotFoundException{
         File archivo = new File (ruta);
         FileReader fr = new FileReader (archivo);
-        flujo = new BufferedReader(fr);
+        bf = new BufferedReader(fr);
     }
     
     public void parameters(){
-         
+        //SUPONIENDO QUE FICHERO = ASF, NO?
+        int tam_registros=66; // 66 bytes ( CABECERA + DATOS MIEMBRO DE NUESTRO REGISTRO)
+        System.out.println("NUMERO DE BLOQUES -> "+(num_reg*tam_registros)/tam_bloq);
+        System.out.println("NUMERO DE CABECERAS DE BLOQUE ->"+((num_reg*tam_registros)/tam_bloq)*tam_cab_bloq);
+        System.out.println("NUMERO CABECERAS REGISTROS ->"+(registros.size()));
     }
     
-    
     public NuestroRegistro first(){
-        return registros.get(0);
+        NuestroRegistro nr = new NuestroRegistro(registros.get(0).isEstado(), registros.get(0).getId(), registros.get(0).getVal1(), 
+        registros.get(0).getVal2(), registros.get(0).getVal3(), registros.get(0).getVal4());
+        return nr;
     }
     
     public NuestroRegistro next() {
-        pos_actual++;
-        return registros.get(pos_actual); 
+        NuestroRegistro nr = new NuestroRegistro(registros.get(pos_actual).isEstado(), registros.get(pos_actual).getId(), 
+        registros.get(pos_actual).getVal1(), registros.get(pos_actual).getVal2(), registros.get(pos_actual).getVal3(), 
+        registros.get(pos_actual).getVal4());
+        pos_actual++;//se incrementa la posicion actual
+        return nr;
     } 
     
-    public NuestroRegistro end(){
-        return registros.get(registros.size()-1);
+    public boolean end(){
+        if(pos_actual==registros.size()-1){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
     
     public void add(NuestroRegistro reg){
@@ -58,7 +92,7 @@ public class NuestroASF {
     public void search(int id){
         boolean salir=false;
         for(int i=0; i<registros.size() && !salir; i++){
-            if(registros.get(id).getId()==id){
+            if(registros.get(i).getId()==id){
                 pos_actual=i;
                 salir=true;
             }
@@ -66,39 +100,47 @@ public class NuestroASF {
     }
     
     public void modify(NuestroRegistro nuevo){
-        registros.add(pos_actual, nuevo);
+        if(pos_actual<0){
+            registros.remove(pos_actual);
+            registros.add(pos_actual,nuevo);
+            pos_actual++; // ?
+        }
     }
     
     public void delete(){
         registros.remove(pos_actual);
-    }
-    
-    public void list() throws IOException{
-        if(flujo.ready()){
-            for(int i=0; i<registros.size(); i++){
-                String val = "Registro " + i + "-> " + registros.get(i).getAnotacion() + registros.get(i).isEstado() + registros.get(i).getId() + registros.get(i).getVal1() + registros.get(i).getVal2() +
-            registros.get(i).getVal3() + registros.get(i).getVal4() + registros.get(i).getVal5();
-                System.out.print(val);
-            }
-            
-        }
+        pos_actual--; // ?
     }
     
     public void list_raw() throws IOException{
         if(flujo.ready()){
             for(int i=0; i<registros.size(); i++){
+                String val = "Registro " + i + "-> " + registros.get(i).isEstado() + registros.get(i).getId() + 
+                registros.get(i).getAnotacion().toString() + registros.get(i).getVal1() + registros.get(i).getVal2() +
+                registros.get(i).getVal3() + registros.get(i).getVal4();
+                System.out.println (val);
+            }
+            
+        }
+    }
+    
+    public void list() throws IOException{
+        if(flujo.ready()){
+            for(int i=0; i<registros.size(); i++)
+            {
                 String val = "Registro " + i + "-> " + registros.get(i).getVal1() + registros.get(i).getVal2() +
-            registros.get(i).getVal3() + registros.get(i).getVal4() + registros.get(i).getVal5();
-                System.out.print(val);
+                registros.get(i).getVal3() + registros.get(i).getVal4();
+                System.out.println(val);
             } 
         }
-        }
+    }
         
     public void close() throws IOException{
        flujo.close();
     }
     
-    public void reorganize(String ruta){
+    public void reorganize(String ruta){ //TABLE SPACE
+        open(ruta);
         
     }
     
--- src/abd_p1/NuestroRegistro.java
+++ src/abd_p1/NuestroRegistro.java
@@ -1,28 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package abd_p1;
 
-/**
- *
- * @author Alberto
- */
 public class NuestroRegistro {
     
-    private boolean estado;
-    private char id;
+    private byte estado;
+    private byte id;
     
     // Dato
     private char[] anotacion;
     private int val1;
     private int val2;
-    private int val3;
     private double val4;
     private double val5;
 
-    public NuestroRegistro(boolean estado, char id, int val1, int val2, int val3, double val4, double val5) {
+    public NuestroRegistro(byte estado, byte id, int val1, int val2, double val3, double val4) {
         this.anotacion = new char[40];
         this.estado = estado;
         this.id = id;
@@ -30,7 +20,6 @@ public class NuestroRegistro {
         this.val2 = val2;
         this.val3 = val3;
         this.val4 = val4;
-        this.val5 = val5;
     }
 
     public NuestroRegistro() {
@@ -40,7 +29,7 @@ public class NuestroRegistro {
         this.estado = estado;
     }
 
-    public void setId(char id) {
+    public void setId(byte id) {
         this.id = id;
     }
 
@@ -56,16 +45,12 @@ public class NuestroRegistro {
         this.val2 = val2;
     }
 
-    public void setVal3(int val3) {
+    public void setVal3(double val3) {
         this.val3 = val3;
     }
 
     public void setVal4(double val4) {
         this.val4 = val4;
-    }
-
-    public void setVal5(double val5) {
-        this.val5 = val5;
     }
 
     public boolean isEstado() {
@@ -88,15 +73,11 @@ public class NuestroRegistro {
         return val2;
     }
 
-    public int getVal3() {
-        return val3;
-    }
-
-    public double getVal4() {
+    public double getVal3() {
         return val4;
     }
 
-    public double getVal5() {
+    public double getVal4() {
         return val5;
     }
 
